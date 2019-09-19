84ab70e4e8d260f07af4951676cec2aac32ddb29
Katsumasa Kitajima <k.k4.bky@gmail.com>
2019-09-19 21:41:58 +0900

    enabled roslaunch and gazebo world file highlighting in init.el for linux

--- emacs/linux/init.el
+++ emacs/linux/init.el
@@ -76,6 +76,13 @@
 ;; .h ファイルを C++ モードで開く
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
+; roslaunch highlighting
+(add-to-list 'auto-mode-alist '("\\.launch$" . xml-mode))
+
+; gazebo world file highlighting
+(add-to-list 'auto-mode-alist '("\\.world$" . xml-mode))
+
+
 
 ;; (require 'rainbow-delimiters)
 ;; (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
@@ -86,3 +93,18 @@
 ;; (set-fontset-font
 ;;  nil 'japanese-jisx0208
 ;;  (font-spec :family "Ricty"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Ricty" :foundry "PfEd" :slant normal :weight normal :height 102 :width normal)))))
+
+
+
